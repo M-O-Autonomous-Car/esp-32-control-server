76b226d238e9ed0811d19465db86ec44ba005f19
Bill Yin <billyin0702@outlook.com>
2024-05-26 13:57:14 -0500

    New version of control with A* hybrid

--- src/main.cpp
+++ src/main.cpp
@@ -24,16 +24,20 @@ void setup()
   init_websocket();
   init_web_socket_routing();
   Serial.println("############## Websocket complete ##############");
+
 }
 
 // ############################ Loop ############################
 void loop()
 {
-  // Execute the WebSocket request
-  ws.cleanupClients();
-  // Execute Drive
-  execute_drive();
-
+  // Listen to Serial inputs
+  int serial_state = execute_command();  
+  // Execute Drive if there are no serial inputs
+  if (serial_state ==  0) {
+    // Execute the WebSocket request
+    ws.cleanupClients();
+    execute_drive();
+  }
 }
 
 // ###################### Functions Definition ######################
@@ -106,6 +110,7 @@ void connect_to_wifi()
       username = line.substring(username_title_length);
     }
   }
+
   // Connect to Wi-Fi network with SSID and password, see if it is WPA2
   Serial.println("Connecting to WiFi with the following credentials:");
   Serial.println("SSID: " + ssid);
@@ -172,9 +177,111 @@ void init_websocket() {
   server.addHandler(&ws);
 }
 
+// Listen to the Serial inputs
+Numbers listen_to_serial()
+{
+  Numbers numbers; 
+
+  if (Serial.available() > 0)
+  {
+    // The object will be a tuple object from python
+    String serial_input = Serial.readString();
+    numbers = parse_serial_input(serial_input);
+
+    // Modify the drive and steer status based on the serial input
+    if (numbers.time_delta > 0)
+    {
+      drive_status = 1;
+    }
+    else if (numbers.time_delta < 0)
+    {
+      drive_status = 2;
+    }
+    else
+    {
+      drive_status = 0;
+    }
+
+    // Modify the angle based on the serial input
+    goal_angle = numbers.new_angle;
+    if (goal_angle < MIN_ANGLE)
+    {
+      goal_angle = MIN_ANGLE;
+    }
+    else if (goal_angle > MAX_ANGLE)
+    {
+      goal_angle = MAX_ANGLE;
+    }
+  } else {
+    numbers.new_angle = DEF_ANGLE;
+    numbers.time_delta = NO_SERIAL_INPUT;
+  }
+
+  return numbers;
+}
+
+// Send ackknowledgement to the serial
+void send_ack(Numbers command)
+{
+  Serial.print(goal_angle);
+  Serial.print(", ");
+  Serial.println(command.time_delta);
+  // Serial.println(seq_num);
+  // seq_num++;
+}
+
+// Parse the serial input
+Numbers parse_serial_input(String input)
+{
+  // This will be a long string with multiple, every comma will be a new value of the following structure
+  // new_angle, time_delta
+  Numbers numbers;
+  int comma_index = input.indexOf(",");
+  numbers.new_angle = input.substring(0, comma_index).toFloat();
+  numbers.time_delta = input.substring(comma_index + 1).toFloat();
+
+  return numbers;
+}
+
+// Execute the command given from the serial
+int execute_command()
+{
+  Numbers command = listen_to_serial();
+
+  if (command.new_angle == DEF_ANGLE && command.time_delta == NO_SERIAL_INPUT)
+  {
+    return 0;
+  }
+
+  int counter = 0;
+  int counter_limit = command.time_delta * 1000;
+  Serial.print("Counter limit: ");
+  Serial.print(counter_limit);
+  Serial.print("  ");
+
+  // Execute the command by executing drive and steer
+  execute_steer();
+  execute_steer(); // twice to ensure the servo is in the correct position, to different angles
+  while (counter <= counter_limit)
+  {
+    execute_drive();
+    // Delay for driving
+    counter += ONE_DELAY;
+  }
+  drive_status = 0; // reset
+
+  // Send acknowledgement to the serial
+  send_ack(command);
+  return 1;
+}
+
 // Turn the servo
 void servo_turn(int angle) {
-  myservo.write(SERVO_PIN, angle);
+  pulsewidth=map(angle,0,180,500,2500);   //将0°- 180°范围映射到500 - 2500范围（即角度到占空比的转换）
+  digitalWrite(SERVO_PIN, HIGH); 
+  delayMicroseconds(pulsewidth);
+  digitalWrite(SERVO_PIN, LOW);
+  delay(20-pulsewidth/1000);
 }
 
 // Write data array to the I2C
@@ -194,51 +301,57 @@ bool WireWriteDataArray(uint8_t reg, int8_t *val, unsigned int len)
     return true;
 }
 
+// Limit the angle to the min and max angle
+int limit_angle(int angle)
+{
+  if (angle < MIN_ANGLE)
+  {
+    return MIN_ANGLE;
+  }
+  else if (angle > MAX_ANGLE)
+  {
+    return MAX_ANGLE;
+  }
+  return angle;
+}
+
+void execute_steer() {
+  // Steer the car based on the steer status
+  // Limit the angle to the min and max angle
+  // Steer_status = 0, means it is auto steering
+  if (steer_status == 0) {
+    goal_angle = limit_angle(goal_angle);
+    servo_turn(goal_angle);
+  } else if (steer_status == 1)
+  {
+    if (goal_angle > MIN_ANGLE) {
+      goal_angle -= turn_speed;
+      servo_turn(goal_angle);
+    }
+  }
+  else if (steer_status == 2)
+  {
+    if (goal_angle < MAX_ANGLE) {
+      goal_angle += turn_speed;
+      servo_turn(goal_angle);
+    }
+  }
+}
+
 // Execute the drive
 void execute_drive()
 {
   // Drive the car based on the drive status
   if (drive_status == 1)
   {
-    Serial.println("Driving forward");
-    WireWriteDataArray(MOTOR_FIXED_SPEED_ADDR, car_forward, 4);
-    delay(10);
+    forward();
   }
   else if (drive_status == 2)
   {
-    Serial.println("Driving backward");
-    WireWriteDataArray(MOTOR_FIXED_SPEED_ADDR, car_back, 4);
-    delay(10);
+    backward();
   }
   else {
-    // Stop moving
-    WireWriteDataArray(MOTOR_FIXED_SPEED_ADDR, car_stop, 4);
-    delay(10);
-  }
-
-  // Steer the car based on the steer status
-  if (steer_status == 1)
-  {
-    Serial.print("Turning left angle: ");
-    Serial.println(curr_angle);
-    if (curr_angle > MIN_ANGLE) {
-      curr_angle -= turn_speed;
-      servo_turn(curr_angle);
-      delay(10);
-    }
-  }
-  else if (steer_status == 2)
-  {
-    Serial.print("Turning right angle:");
-    Serial.println(curr_angle);
-    if (curr_angle < MAX_ANGLE) {
-      curr_angle += turn_speed;
-      servo_turn(curr_angle);
-      delay(10);
-    }
-  }
-  else {
-    // Serial.println("No Steer input");
+    stop_drive();
   }
 }
 
@@ -249,15 +362,11 @@ void drive_pins_setup()
   pinMode(I2C_SCL, OUTPUT);
   pinMode(I2C_SDA, OUTPUT);
   pinMode(output48, OUTPUT); // LED
+  pinMode(SERVO_PIN, OUTPUT);
 }
 
 // Notify the clients
 void notifyClients() {
-  Serial.println("Drive Status: ");
-  Serial.println(drive_status);
-  Serial.println("Steer Status: ");
-  Serial.println(steer_status);
-
   ws.textAll(String(drive_status) + String(steer_status));
 }
 
@@ -270,17 +379,21 @@ void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
     Serial.println((char*)data);
     Serial.println("");
     if (strcmp((char*)data, "left") == 0) {
-      left();
+      steer_status = 1;
     } else if (strcmp((char*)data, "right") == 0) {
-      right();
+      steer_status = 2;
     } else if (strcmp((char*)data, "forward") == 0) {
-      forward();
+      drive_status = 1;
     } else if (strcmp((char*)data, "backward") == 0) {
-      backward();
+      drive_status = 2;
     } else if (strcmp((char*)data, "not left") == 0 || strcmp((char*)data, "not right") == 0){
       steer_status = 0;
     } else if (strcmp((char*)data, "not forward") == 0 || strcmp((char*)data, "not backward") == 0){
       drive_status = 0;
+    } else if (strcmp((char*)data, "recenter") == 0) {
+      Serial.println("Recentering the servo...");
+      goal_angle = DEF_ANGLE;
+      steer_status = 0;
     }
     notifyClients(); // Notify the clients
   }
@@ -315,44 +428,39 @@ void init_web_socket_routing() {
 // ###################### Drive Controls Functions ######################
 void forward()
 {
-  Serial.println("Driving forward");
   drive_status = 1;
-  turn_on_led();
+  WireWriteDataArray(MOTOR_FIXED_SPEED_ADDR, car_forward, 4);
+  delay(ONE_DELAY);
 }
 
 void backward()
 {
-  Serial.println("Driving backward");
   drive_status = 2;
-  turn_on_led();
+  WireWriteDataArray(MOTOR_FIXED_SPEED_ADDR, car_back, 4);
+  delay(ONE_DELAY);
 }
 
 void left()
 {
-  Serial.println("Turning left");
-  steer_status = 1;
-  turn_on_led();
+  servo_turn(goal_angle);
 }
 
 void right()
 {
-  Serial.println("Turning right");
-  steer_status = 2;
-  turn_on_led();
+  servo_turn(goal_angle);
 }
 
 void stop_drive()
 {
-  Serial.println("Stopping drive");
   drive_status = 0;
-  turn_off_led();
+  // Stop moving
+  WireWriteDataArray(MOTOR_FIXED_SPEED_ADDR, car_stop, 4);
+  delay(ONE_DELAY);
 }
 
 void stop_steer()
 {
-  Serial.println("Stopping steer");
   steer_status = 0;
-  turn_off_led();
 }
 
 // ###################### GPIO Functions ######################
--- src/main.h
+++ src/main.h
@@ -14,6 +14,7 @@
 // 1. Webserverl ibrary from arduino
 // 2. Webserver.h
 // 3. Elegant OTA does not work on Eduroam
+// 4. TAKSs ON ESP32 (won't interfere)
 
 // ###################### Structs ######################
 typedef struct
@@ -21,6 +22,11 @@ typedef struct
     File file;
     int status;
 } FileStruct;
+
+typedef struct {
+    float new_angle;
+    float time_delta;
+} Numbers;
 
 // ###################### Functions Declaration ######################
 void turn_high_gpio_pin(int pin);
@@ -37,7 +43,13 @@ void read_html_file();
 void connect_to_wifi();
 void drive_pins_setup();
 void servo_turn(int angle);
+void execute_steer();
 void execute_drive();
+
+int limit_angle(int angle);
+Numbers listen_to_serial();
+Numbers parse_serial_input(String input);
+int execute_command();
 
 bool WireWriteDataArray(uint8_t reg, int8_t *val, unsigned int len);
 void turn_on_led();
@@ -83,26 +95,33 @@ const int output48 = 48;
 // Car Variables
 #define MOTOR_FIXED_SPEED_ADDR    51 //
 #define MOTOR_I2C_ADDR 0x34
-#define I2C_SCL 20
-#define I2C_SDA 21
+#define I2C_SCL 47 // orange wire
+#define I2C_SDA 45 // yellow wire
 #define SERVO_PIN 15
+int pulsewidth;     //占空比
 
 int8_t car_forward[4]={16,0,-16,0};   // forward
 int8_t car_back[4]={-16,0,16,0};  // backward
 int8_t car_stop[4]={0,0,0,0};
 
 // Drive variables
-int drive_status = 0; // 0: Not in drive, 1: Forward, 2: Left
+#define ONE_DELAY 10 // ms
+#define TURN_DELAY 100
+#define DELAY_DELAY 100
+#define NO_SERIAL_INPUT 0
+int drive_status = 0; // 0: Not in drive, 1: Forward, 2: Backward
 int steer_status = 0; // 0: Not steering, 1: Left, 2: Right
 int drive_speed = 25;
 
+int seq_num = 0;
+
 // Servo variables
-Servo myservo = Servo();
 #define MAX_ANGLE 135
 #define MIN_ANGLE 45
 #define DEF_ANGLE 90
-const int turn_speed = 1; // Minimum is 1
+const int turn_speed = 2; // Minimum is 1
 int curr_angle = 90;
+int goal_angle = 90;
 
 // HTML web page variable
 String html_code_str;
